500d92d63ae2088f2cf682ac395ea44c9ac8c71b
saule <saule2007@gmail.com>
2018-06-28 01:26:28 +0300

    add share menu option

--- app/src/main/java/qazaqkeyboard/kz/aygolek/abcnetkeyboard/MainActivity.java
+++ app/src/main/java/qazaqkeyboard/kz/aygolek/abcnetkeyboard/MainActivity.java
@@ -1,15 +1,26 @@
 package qazaqkeyboard.kz.aygolek.abcnetkeyboard;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.support.v7.widget.ShareActionProvider;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ShareActionProvider shareActionProvider=null;
+
+    private Intent shareIntent = new Intent(Intent.ACTION_SEND);
+
     private Button btn_select_kbd;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,5 +35,31 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        shareIntent.setType("text/plain");
+        shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        shareIntent.putExtra(Intent.EXTRA_TEXT,"#abcnet.kz");
+
+
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.detailfragment, menu);
+        // Locate MenuItem with ShareActionProvider
+        MenuItem shareMenu = menu.findItem(R.id.action_share);
+
+        shareActionProvider= (ShareActionProvider) MenuItemCompat.getActionProvider(shareMenu);
+        Log.d("saule","before");
+        if(shareActionProvider!=null) {
+            shareActionProvider.setShareIntent(shareIntent);
+        }
+        else {
+            Log.d("saule","shareactionporvider null");
+        }
+
+        return true;
+    }
+
+
 }
